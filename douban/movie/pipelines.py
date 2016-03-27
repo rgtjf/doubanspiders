99d557ed42b5bb79f0bbe427c00be577f2c93c78
rgtjf <rgtjf@users.noreply.github.com>
2016-03-27 15:59:21 +0800

    Update pipelines.py

--- douban/movie/pipelines.py
+++ douban/movie/pipelines.py
@@ -3,11 +3,16 @@
 
 
 class MoviePipeline(object):
+    
+    def __init__(self):
+        self.file = open('items.json', 'w')
+    
     def process_item(self, item, spider):
         #if spider.name != "movie":  return item
         #if item.get("subject_id", None) is None: return item
 
         #spec = { "subject_id": item["subject_id"] }
         #doubanDB.movie.update(spec, {'$set': dict(item)}, upsert=True)
-
+        line = json.dumps(dict(item)) + '\n'
+        self.file.write(line)
         return item
